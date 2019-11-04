ca0c6ce5f22ec940c8991f7461ce69078aad0784
Elliekmultimedia <Elliekmultimedia@gmail.com>
2019-11-04 11:16:39 -0500

    setting view engine to ejs and creating views folder for users

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
  
+//setting view engine
+app.set('view engine', 'ejs');
+
+/*
+    Routes
+*/
+
+//GET/
 app.get('/', function (req, res) {
-  res.send('Hello')
+  res.render('home.ejs');
 })
  
 app.listen(3000, function(){
