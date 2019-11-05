da03fe327bfcce17cb0bfbb29efd01544f158a94
Elliekmultimedia <Elliekmultimedia@gmail.com>
2019-11-05 10:49:21 -0500

    Installing body-parser

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # weather-now
+
+get user input of the city 
+
+get the weather for the city
+    if err let the user know there was an err and try again
+    if thereis no weather availible err try again
+    else store the weather for the user city
+show the weather or err
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const app = express();
+const bodyParser = require('body-parser');
  
+const app = express();
 //setting view engine
 app.set('view engine', 'ejs');
 
 //middleware
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended:false}));
 
 /*
     Routes
@@ -14,8 +16,12 @@ app.use(express.static('./public'));
 //GET/
 app.get('/', function (req, res) {
   res.render('home.ejs');
-})
+});
  
+app.post('/whatever-we-want', function(req,res){
+  console.log(req);
+})
+
 app.listen(3000, function(){
     console.log('server is live on port: 3000')
 });
