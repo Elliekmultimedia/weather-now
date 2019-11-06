982e7712eb9b41c945fe28465389035db1035ed4
Elliekmultimedia <Elliekmultimedia@gmail.com>
2019-11-06 15:52:19 -0500

    Weather-Now

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser');
- 
+const request = require('request');
+
 const app = express();
+
+const apiKey = process.env.APIKEY;
+
+console.log(process.env.APIKEY)
 //setting view engine
 app.set('view engine', 'ejs');
 
@@ -14,15 +20,27 @@ app.use(bodyParser.urlencoded({extended:false}));
     Routes
 */
 
-//GET/
+//GET ROOT/
 app.get('/', function (req, res) {
-  res.render('home.ejs');
+  
+  res.render('home.ejs', { weather: null, error: null });
 });
- 
-app.post('/', function(req,res){
-  console.log(req.body.city);
-  res.end();
+
+// handling post request for root
+app.post('/', function(req,res) {
+  let url = "http://api.openweathermap.org/data/2.5/weather?q=" + req.body.location
+  
+  res.render('home.ejs', {weather: weatherNow, error: err});
+  
+  request(function(error, response, body) {
+  console.log('error', error);
+  console.log('statusCode', response && response.statusCode);
+  console.log('body:', body)
+
 })
+})
+
+
 
 app.listen(3000, function(){
     console.log('server is live on port: 3000')
